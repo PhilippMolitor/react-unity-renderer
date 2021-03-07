be3442115de773266b548d74f087eb89a600d3c7
Philipp Molitor <philipp@molitor.cloud>
2021-03-07 15:06:03 +0100

    clean up tests and add type reference for context testing

--- src/lib/__tests__/context.test.ts
+++ src/lib/__tests__/context.test.ts
@@ -1,3 +1,5 @@
+/// <reference path="../../../typings/unity.d.ts" />
+
 import { UnityContext, UnityLoaderConfig } from '../context';
 
 describe('UnityContext', () => {
@@ -16,9 +18,7 @@ describe('UnityContext', () => {
   };
 
   beforeEach(async () => {
-    // @ts-ignore
     delete window.__UnityBridgeRegistry__;
-    // @ts-ignore
     delete window.UnityBridge;
   });
 
@@ -29,37 +29,29 @@ describe('UnityContext', () => {
   });
 
   it('creates the global event registry', async () => {
-    // @ts-ignore
     expect(window.__UnityBridgeRegistry__).toBe(undefined);
 
     new UnityContext(cfg);
 
-    // @ts-ignore
     expect(window.__UnityBridgeRegistry__).toStrictEqual({});
   });
 
   it('creates the global event lookup handler', async () => {
-    // @ts-ignore
     expect(window.UnityBridge).toBe(undefined);
 
     new UnityContext(cfg);
 
-    // @ts-ignore
     expect(typeof window.UnityBridge).toBe('function');
   });
 
   it('registers an event handler to the global registry', async () => {
     const ctx = new UnityContext(cfg);
-
     const callback = jest.fn();
 
     ctx.on('test', (...params: any) => callback(params));
-
     expect(callback).not.toHaveBeenCalled();
 
-    // @ts-ignore
     window.UnityBridge('test')('string', 42);
-
     expect(callback).toHaveBeenCalledWith(['string', 42]);
   });
 });
