05bce028ef1781284ce8752d5c7b0a9864239446
Philipp Molitor <philipp@molitor.cloud>
2021-03-07 15:01:40 +0100

    add some basic event handler tests for UnityContext

--- src/lib/__tests__/context.test.ts
+++ src/lib/__tests__/context.test.ts
@@ -15,9 +15,51 @@ describe('UnityContext', () => {
     productVersion: 'k',
   };
 
+  beforeEach(async () => {
+    // @ts-ignore
+    delete window.__UnityBridgeRegistry__;
+    // @ts-ignore
+    delete window.UnityBridge;
+  });
+
   it('stores and retrieves the loader configuration', async () => {
     const ctx = new UnityContext(cfg);
 
-    expect(ctx.getConfig()).toEqual(cfg);
+    expect(ctx.getConfig()).toStrictEqual(cfg);
+  });
+
+  it('creates the global event registry', async () => {
+    // @ts-ignore
+    expect(window.__UnityBridgeRegistry__).toBe(undefined);
+
+    new UnityContext(cfg);
+
+    // @ts-ignore
+    expect(window.__UnityBridgeRegistry__).toStrictEqual({});
+  });
+
+  it('creates the global event lookup handler', async () => {
+    // @ts-ignore
+    expect(window.UnityBridge).toBe(undefined);
+
+    new UnityContext(cfg);
+
+    // @ts-ignore
+    expect(typeof window.UnityBridge).toBe('function');
+  });
+
+  it('registers an event handler to the global registry', async () => {
+    const ctx = new UnityContext(cfg);
+
+    const callback = jest.fn();
+
+    ctx.on('test', (...params: any) => callback(params));
+
+    expect(callback).not.toHaveBeenCalled();
+
+    // @ts-ignore
+    window.UnityBridge('test')('string', 42);
+
+    expect(callback).toHaveBeenCalledWith(['string', 42]);
   });
 });
